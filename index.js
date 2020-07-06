9e9c327d07c1871cb86ae7ca861eac15911fcf75
Yoon Jung <dannydannyl@me.com>
2020-07-05 22:11:27 -0400

    https only

--- index.js
+++ index.js
@@ -20,8 +20,11 @@ app.set('port', port);
 /**
  * Create HTTP server.
  */
-
-var server = https.createServer(app);
+var httpsOptions = {
+  key: fs.readFileSync("/etc/letsencrypt/live/yo0on.com/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/yo0on.com/fullchain.pem")
+}
+var server = https.createServer(httpsOptions, app);
 
 /**
  * Listen on provided port, on all network interfaces.

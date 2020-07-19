cf5b7e11cde737a7d38ae66c6a8505e1f6fdacd3
Yoon Jung <dannydannyl@me.com>
2020-07-18 22:51:32 -0400

    mobile friendly

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var mobile = require('is-mobile');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  if(mobile){
+  if(mobile()){
     res.render('index_mobile', { title: 'Yoon Jung' });
   }else{
     res.render('index', { title: 'Yoon Jung' });

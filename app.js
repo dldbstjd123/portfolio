02a22f21b4536ee8ee4c8fb7f09ad335d072dde7
Yoon Jung <dannydannyl@me.com>
2020-07-18 22:58:49 -0400

    mobile friendly

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var favicon = require('serve-favicon')
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var device = require('express-device');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,6 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
+app.use(device.capture());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var mobile = require('is-mobile');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log(req.device.type.toUpperCase())
   if(mobile()){
     res.render('index_mobile', { title: 'Yoon Jung' });
   }else{
